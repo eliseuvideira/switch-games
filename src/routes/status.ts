879b0d2b3a6f0bac9d9b23d827c066db6782a386
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-10 15:23:46 -0300

    :tada: initial setup

---
+++ src/routes/status.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { getStatus } from '../endpoints/status';
+
+const router = Router();
+
+/**
+ * GET /status
+ * @tag Status
+ * @response 204
+ * @response default
+ * @responseContent {Error} default.application/json
+ */
+router.get('/status', getStatus);
+
+export default router;
