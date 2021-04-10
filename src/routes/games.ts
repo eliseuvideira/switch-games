298db2a4bd48b1d493d1410e257c39c5fce48f69
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-10 15:28:14 -0300

    :wrench: routes layout done

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-import { getStatus } from '../endpoints/status';
+import { gamesGetMany } from '../endpoints/games';
 
 const router = Router();
 
 /**
- * GET /status
- * @tag Status
+ * GET /games
+ * @tag Games
  * @response 204
  * @response default
  * @responseContent {Error} default.application/json
  */
-router.get('/status', getStatus);
+router.get('/games', gamesGetMany);
 
 export default router;
